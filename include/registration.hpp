dc10a4238dd6f81e7f6e10347a3dd4bce1952711
TimBernard <timothypbernard@gmail.com>
2020-08-24 19:47:02 -0400

    Namespaces, cleanup

--- include/registration.hpp
+++ include/registration.hpp
@@ -4,13 +4,23 @@
 #include <eigen3/Eigen/Dense>
 #include <iostream>
 
-// Function that performs rigid point cloud to point cloud registrations
-Eigen::MatrixXd rigidPointToPointSVD(const Eigen::MatrixX3d& A,  const Eigen::MatrixX3d& B);
+namespace reg{
+  
+  /**
+     Solve for Rigid Transformation between two 3D point sets using Singular Value Decomposition.
 
-// Function that iteratively finds point correspondences and registrations between two point clouds
-//Eigen::MatrixXd icp(const 
-Eigen::MatrixXd icp();
-
-// icp
-
+     See: 
+     Arun KS, Huang TS, Blostein SD. Least-squares fitting of two 3-d point sets. 
+     IEEE Trans Pattern Anal Mach Intell. 1987;9(5):698-700. doi:10.1109/tpami.1987.4767965
+     
+     \param[in] A        Set of 3D points 
+     \param[in] B        Set of 3D points 
+     \return             SE3 Homogeneous Transformation 
+  */
+  Eigen::MatrixXd rigidPointToPointSVD(const Eigen::MatrixX3d& A,  const Eigen::MatrixX3d& B);
+  
+  // Function that iteratively finds point correspondences and registrations between two point clouds
+  Eigen::MatrixXd icp();
+    
+}
 #endif /* REGISTRATION_HPP */
